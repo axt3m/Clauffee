c0a3918a61d92aac7f98d09ae11f0830eeab56d1
Angela Terao <angela.terao@gmail.com>
2026-06-12 09:11:15 +0200

    Initial Commit

---
+++ CaffeLatte/CaffeLatteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaffeLatteApp.swift
+//  CaffeLatte
+//
+//  Created by Angela on 12/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CaffeLatteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
